172f2c7001091f9eab8d5382981d9b4d498c8fbb
xiaohuiuu <1195005422@qq.com>
2023-09-05 14:57:27 +0800

    添加多行外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,17 +75,22 @@ class AlienInvasion:
         """创建一个外星人舰队"""
         # 外星人的间距为外星人的宽度
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+            # 添加一行外星人后，重置x值并递增y值
+            current_x = alien_width
+            current_y += 2 * alien_height
 
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
     def _update_screen(self):
--- alien_invasion开发流程.md
+++ alien_invasion开发流程.md
@@ -616,3 +616,31 @@ def _create_alien(self, x_position):
     self.aliens.add(new_alien)
 ```
 
+### 5 添加多行外星人
+
+ ```python
+ def _create_fleet(self):
+     alien = Alien(self)
+     alien_width, alien_height = alien.rect.size
+     current_x, current_y = alien_width, alien_height
+     while current_y < (self.settings.screen_height -3 * alien_height):
+         while current_x < (self.settings.screen_width -2 * alien_width):
+             self._create_alien(current_x, current_y)
+             current_x += 2 * alien_width
+         
+         # 添加一行外星人后，重置x值并递增y值
+         current_x = alien_width
+         current_y += 2 * alien_height
+ ```
+
+修改\_create_alien()，以正确的设置外星人的垂直位置
+
+```python
+def _create_alien(self, x_position, y_position):
+    new_alien = Alien(self)
+    new_alien.x = x_position
+    new_alien.rect.x = x_position
+    new_alien.rect.y = y_position
+    self.aliens.add(new_alien)
+```
+
